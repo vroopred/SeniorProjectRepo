5c11b3b727db0eb39b4fc0d43ce614f84649eda0
Varsha Roopreddy <vroopred@calpoly.edu>
2016-02-22 10:30:56 -0800

    Initial Commit

---
+++ Senior Project/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Senior Project
+//
+//  Created by Varsha Roopreddy on 2/22/16.
+//  Copyright © 2016 Anusha Praturu & Varsha Roopreddy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
